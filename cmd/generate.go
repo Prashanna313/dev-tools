fb95a03e9af171348991a3bb9c2199b6b20db1df
Prashanna_31 <prashanna.r@idp.com>
2024-07-30 10:24:24 +0530

    add generation code

--- cmd/generate.go
+++ cmd/generate.go
@@ -3,25 +3,43 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/prashanna313/dev-tools/internal/python"
 	"github.com/spf13/cobra"
 )
 
-var language string
-var framework string
+var projectName string
+var templateType string
 
 // generateCmd represents the generate command
 var generateCmd = &cobra.Command{
 	Use:   "generate",
-	Short: "Generate boilerplate code",
-	Long:  `Generate boilerplate code for different languages and frameworks.`,
+	Short: "Generate Python boilerplate code",
+	Long:  `Generate Python boilerplate code based on selected templates such as Flask, Django, or CLI applications.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Generating boilerplate code for %s using %s framework...\n", language, framework)
-		// Add code generation logic here
+		if projectName == "" {
+			fmt.Println("Please specify a project name using the -n or --name flag.")
+			return
+		}
+
+		if !python.IsValidTemplate(templateType) {
+			fmt.Printf("Unknown template type: %s. Available templates are:\n", templateType)
+			for key, value := range python.AvailableTemplates() {
+				fmt.Printf("- %s: %s\n", key, value)
+			}
+			return
+		}
+
+		err := python.GenerateProject(projectName, templateType)
+		if err != nil {
+			fmt.Printf("Error generating project: %v\n", err)
+		} else {
+			fmt.Printf("Successfully generated %s project '%s'.\n", python.AvailableTemplates()[templateType], projectName)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(generateCmd)
-	generateCmd.Flags().StringVarP(&language, "language", "l", "", "Programming language (e.g., Go, Python)")
-	generateCmd.Flags().StringVarP(&framework, "framework", "f", "", "Framework to use")
+	generateCmd.Flags().StringVarP(&projectName, "name", "n", "", "Name of the project")
+	generateCmd.Flags().StringVarP(&templateType, "template", "t", "", "Type of template to generate (flask, django, cli)")
 }
